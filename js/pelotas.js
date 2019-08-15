c46b6db1960a3b1faaf966884722a124abbc4947
Manuel Pascual <manupascual90@gmail.com>
2019-08-15 20:54:06 -0300

    Agrega el método draw a la clase Ball

--- js/pelotas.js
+++ js/pelotas.js
@@ -8,3 +8,10 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+Ball.prototype.draw = function() {
+  ctx.beginPath();
+  ctx.fillStyle = this.color;
+  ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  ctx.fill();
+}
