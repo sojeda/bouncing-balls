7f06b26f6b3d4259478680194210ac55b83d7250
Gabriel <gabriel88944@hotmail.com>
2019-08-15 21:00:42 -0300

    Ball Class

--- js/pelotas.js
+++ js/pelotas.js
@@ -8,3 +8,14 @@ function random(min,max) {
   var num = Math.floor(Math.random()*(max-min)) + min;
   return num;
 }
+
+class Ball {
+	constructor(x, y, velX, velY, color, size) {
+		this.x = x;
+		this.y = y;
+		this.velX = velX;
+		this.velY = velY;
+		this.color = color;
+		this.size = size;
+	}
+}
