8b703786f39ed465c1b468f30910eecdbd876442
tefanga <stef.plm@hotmail.com>
2019-08-15 21:10:37 -0300

    added some fixes

--- js/pelotas.js
+++ js/pelotas.js
@@ -4,22 +4,22 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 // function to generate random number
-function random(min,max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
+function random(min, max) {
+  var num = Math.floor(Math.random() * (max - min)) + min;
   return num;
 }
 
 class Ball {
-	constructor(x, y, velX, velY, color, size) {
-		this.x = x;
-		this.y = y;
-		this.velX = velX;
-		this.velY = velY;
-		this.color = color;
-		this.size = size;
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
-  
-  draw(){
+
+  draw() {
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
