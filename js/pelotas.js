b96834b236eae04be28dd653536f60a2d3959add
Gabriel <gabriel88944@hotmail.com>
2019-08-27 20:30:12 -0300

    cambio Ball por Shape

--- js/pelotas.js
+++ js/pelotas.js
@@ -20,7 +20,9 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
-class Ball {
+
+
+class Shape {
    
    constructor(x, y, velX, velY, color, size) {
       this.x = x;
