ee8414f38bcd06a73a5b403f3f2cbf675e5c761b
Manuel Pascual <manupascual90@gmail.com>
2019-08-27 20:44:18 -0300

    Modifica propiedades de la clase Shape y clase Ball

--- js/pelotas.js
+++ js/pelotas.js
@@ -22,13 +22,11 @@ var height = canvas.height = window.innerHeight;
 
 class Shape {
    
-   constructor(x, y, velX, velY, color, size) {
+   constructor(x, y, velX, velY) {
       this.x = x;
       this.y = y;
       this.velX = velX;
       this.velY = velY;
-      this.color = color;
-      this.size = size;
    }
    
    draw() {
@@ -76,9 +74,10 @@ class Shape {
 }
 
 class Ball extends Shape{
-   constructor(){
-      super(x, y, velX, velY, color, size);
-
+   constructor(x, y, velX, velY, color, size){
+      super(x, y, velX, velY);
+      this.color = color;
+      this.size = size;
    }
 }
 
