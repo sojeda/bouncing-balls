015a1fe5c7ae333d5cdb05f9df74d0d5f69b998f
lautacampos <lauta.isa.campos@gmail.com>
2019-08-27 20:23:57 -0300

    Se modifico el nombre de la clase balls por Shape y se creo un nuevo objeto heredando el constructor de Shape

--- js/pelotas.js
+++ js/pelotas.js
@@ -20,7 +20,7 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width = window.innerWidth;
 var height = canvas.height = window.innerHeight;
 
-class Ball {
+class Shape {
    
    constructor(x, y, velX, velY, color, size) {
       this.x = x;
@@ -74,6 +74,13 @@ class Ball {
    }
    
 }
+
+class Ball extends Shape {
+   super(){
+      
+   }
+}
+
 
 var balls = [];
 
